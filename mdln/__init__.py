bd43008df80765a825cd9e487a8e289ad08ccfa6
madeline <qwertytrogi@gmail.com>
2023-07-07 18:39:42 -0500

    license and setup

--- mdln/__init__.py
+++ mdln/__init__.py
@@ -1,5 +1,12 @@
+"""
+A lightweight, high performance, and object-focused 2D game framework in Python
+"""
+
 from .const import *
 from .entity import *
 from .game import *
 from .scene import *
 from .stage import *
+
+__author__ = "Madeline"
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import sys
+
+if sys.version_info < (3, 6):
+    raise ImportError("MDLN requires Python 3.6 or later.")
+
+import mdln
+
+setuptools.setup(
+    name="mdln",
+    version=mdln.__version__,
+    author=mdln.__author__,
+    description=mdln.__doc__.replace("\n", " ").strip(),
+    license="MIT",
+    keywords="mdln game engine pygame entity component framework",
+    url="https://github.com/qwertyquerty/MDLN",
+    packages=["mdln"],
+    test_suite="test",
+    python_requires=">=3.6",
+    project_urls={
+        "Documentation": "https://github.com/qwertyquerty/MDLN",
+    }
+)
