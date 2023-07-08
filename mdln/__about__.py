6cab05359abdd99073c78cd306c3941f58e100cc
madeline <qwertytrogi@gmail.com>
2023-07-07 19:12:59 -0500

    0.1.1

--- mdln/__about__.py
+++ mdln/__about__.py
@@ -4,5 +4,5 @@ A lightweight, high performance, and object-focused 2D game framework in Python
 
 __title__ = "mdln"
 __author__ = "Madeline"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __license__ = "MIT"
